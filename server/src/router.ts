81287d3a964a49540ddd4de15af254eef9c8eb50
theo <boronattheo83@gmail.com>
2025-01-06 11:03:14 +0100

    routing avancé

--- server/src/router.ts
+++ server/src/router.ts
@@ -13,14 +13,22 @@ router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
 
+import categoryActions from "./modules/category/categoryActions";
 // Define program-related routes
 import programActions from "./modules/programs/ProgramAction";
 
 router.get("/api/programs", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
+router.get("/api/categories", categoryActions.browse);
+router.get("/api/categories/:id", categoryActions.read);
 
 /* ************************************************************************* */
 
 // Declaration of a "Welcome" route
+
+import SayAction from "./modules/say/SayAction";
+
+router.get("/", SayAction.sayWelcome);
 
 /* ************************************************************************* */
 
